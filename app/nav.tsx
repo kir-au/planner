90e9d56b68bdfbe314936ff350c910553c266f00
Kir <kirill.frolov@gmail.com>
2025-12-29 02:17:28 +1100

    Fixed the build error by removing the duplicate default export in nav.tsx. Now the file only exports Nav and uses the imported Navbar

--- app/nav.tsx
+++ app/nav.tsx
@@ -6,15 +6,3 @@ export default async function Nav() {
   const session = await getServerSession();
   return <Navbar user={session?.user} />;
 }
-
-export default function Navbar() {
-  return (
-    <nav className="flex items-center justify-between p-4">
-      <a href="/planner" className="flex items-center gap-2">
-        <PlannerMark className="h-6 w-6" />
-        <span className="hidden sm:inline font-semibold">Planner</span>
-      </a>
-      {/* ...other navbar content... */}
-    </nav>
-  );
-}
